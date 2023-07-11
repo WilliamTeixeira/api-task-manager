f11aeffb0561eaaae2ea13f809cc9b15a7d7d569
William Teixeira <tx.william@gmail.com>
2023-07-10 22:33:45 -0300

    refactor: Union of migration v3 with migration v2

--- src/main/resources/db/migration/V2__create-table-user.sql
+++ src/main/resources/db/migration/V2__create-table-user.sql
@@ -3,7 +3,7 @@ create table users(
     id bigint not null auto_increment,
     username varchar(100) not null,
     password varchar(255) not null,
+    roles varchar(255) not null,
 
     primary key(id)
-
 );
