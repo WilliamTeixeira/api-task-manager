d3d54c46f1678cd065d5e0bfd0ef91d25c1bd319
William Teixeira <tx.william@gmail.com>
2023-07-12 22:05:07 -0300

    feat: Add user default Admin with migration

---
+++ src/main/resources/db/migration/V6__insert-admin-into-table-user.sql
@@ -0,0 +1,6 @@
+
+insert into users (username, password, roles)
+    values (
+        "admin@taskmanager.com",
+        "$2a$10$sVbDAO.tuVDBXX8Ivdts3.nfT4CZ3h9jKQPvyNnZhjOF64hZUhlue",
+        "ROLE_ADMIN, ROLE_USER");
