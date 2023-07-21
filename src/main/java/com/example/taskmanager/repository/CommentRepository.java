2b8034eafc5d316e7f0549a66ed63ef4e5b031d9
William Teixeira <tx.william@gmail.com>
2023-07-20 23:14:54 -0300

    feat: Add find method for CommentRepository

--- src/main/java/com/example/taskmanager/repository/CommentRepository.java
+++ src/main/java/com/example/taskmanager/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.example.taskmanager.repository;
 
 import com.example.taskmanager.domain.comment.Comment;
+import com.example.taskmanager.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByTask(Task task);
 }
